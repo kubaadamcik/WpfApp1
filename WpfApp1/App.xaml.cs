068ec29e2bef81d942450a638277c4ec98fc232f
Jakub Adamčík <59259adamcik@student.sstebrno.cz>
2024-09-19 21:40:17 +0200

    Přidejte soubory projektu.

---
+++ WpfApp1/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Demo;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+
+}
+
+public class User
+{
+    public string username;
+    public string password;
+}
+
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public string username;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void UsernameGotFocus(object sender, RoutedEventArgs e)
+    {
+        EnterUsername.Text = string.Empty;
+
+        if (EnterPassword.Text != string.Empty && EnterPassword.Text != "Enter your password")
+        {
+            LoginButton.IsEnabled = true;
+        }
+    }
+
+    private void PasswordGotFocus(object sender, RoutedEventArgs e)
+    {
+        EnterPassword.Text = string.Empty;
+
+        if (EnterUsername.Text != string.Empty && EnterUsername.Text != "Enter your password")
+        {
+            LoginButton.IsEnabled = true;
+        }
+    }
+
+    private void CreateAccount(object sender, RoutedEventArgs e)
+    {
+        if (EnterUsername.Text != string.Empty && EnterUsername.Text != "Enter your password" && EnterPassword.Text != string.Empty && EnterPassword.Text != "Enter your password")
+        {
+            User user = new User();
+
+            user.username = EnterUsername.Text;
+            user.password = EnterPassword.Text;
+
+            /*
+             MainGrid.Children.Remove(EnterUsername);
+            MainGrid.Children.Remove(EnterPassword);
+            MainGrid.Children.Remove(LoginButton);
+            */
+
+            MessageBox.Show($"Your username is {user.username} and your password is {user.password}");
+        }
+    }
+}
